5d0cc5e3dfa0297add728456abc892b45b0bb5ea
KIRUBANITHI V <141173903+KIRUBANITHI-V@users.noreply.github.com>
2023-11-16 12:43:58 +0530

    Operators task

---
+++ Operators.java
@@ -0,0 +1,16 @@
+package RailWay;
+
+public class Operators {
+public static void main(String[] args) {
+	int a = 10;
+	int b = 20;
+	int c = 30;
+	
+  System.out.println(a+"+"+b+" = "+(a+b));
+  System.out.println(a+"-"+b+" = "+(a-b));
+  System.out.println(a+"*"+b+" = "+(a*b));
+  System.out.println(b+"/"+a+" = "+(b/a));
+  System.out.println(a+"%"+b+" = "+(a%b));
+  System.out.println(a+"+"+b+"*"+c+" = "+(a+b)*c);
+}
+}
